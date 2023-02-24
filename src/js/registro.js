f599173e0725439dff5580177622b3af2f753e8b
juanmejiab <juanmejiab02@gmail.com>
2023-02-23 22:06:23 -0500

    Implementacion del registro

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,4 +1,4 @@
-let secciones = ["opciones.html", "inicio.html"]
+let secciones = ["login.html", "inicio.html"]
 let btnRegistrarse, logoRegistro;
 
 window.onload = init;
@@ -14,8 +14,8 @@ function inicializarVariables(){
 }
 
 function crearEventos(){
-    btnRegistrarse.addEventListener("click", enrutador);
     logoRegistro.addEventListener("click", enrutador);
+    btnRegistrarse.addEventListener("click", registrar);
 }
 
 function enrutador(evento){
@@ -25,4 +25,40 @@ function enrutador(evento){
     else{
         location.href = secciones[1];
     }    
+}
+
+function registrar(){
+    let nombre = document.getElementById("nombre");
+    let apellidos = document.getElementById("apellidos");
+    let fechaNacimiento = document.getElementById("fechaNacimiento");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+
+    if (calcularEdad(fechaNacimiento.value)>=18){
+        let usuario = {
+            nombre:nombre.value,
+            apellidos:apellidos.value,
+            fechaNacimiento:fechaNacimiento.value,
+            email:email.value,
+            password:password.value
+        };
+    
+        localStorage.setItem("usuario",JSON.stringify(usuario));
+        alert("Registro exitoso!");
+        location.href=secciones[0];
+    }
+    else{
+        alert("Para registrarse debe ser mayor de edad");
+    }
+    
+}
+function calcularEdad(fecha){
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+    return edad;
 }
