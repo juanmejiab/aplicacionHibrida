61b373d421933044f89646a4f013243d7c7c64f5
juanmejiab <juanmejiab02@gmail.com>
2023-02-24 23:14:57 -0500

    cambios en estilos de la pagina de mesas

--- src/js/mesas.js
+++ src/js/mesas.js
@@ -1,5 +1,5 @@
 let secciones = "opciones.html"
-let logoMesas
+let logoMesas, btnMesas;
 
 window.onload = init;
 
@@ -10,12 +10,18 @@ function init(){
 
 function inicializarVariables(){
     logoMesas = document.getElementById("logoMesas");
+    btnMesas = document.getElementById("mesa");
 }
 
 function crearEventos(){
     logoMesas.addEventListener("click", enrutador);
+    btnMesas.addEventListener("click", prueba);
 }
 
 function enrutador(){
     location.href = secciones;
+}
+
+function prueba(){
+    //alert(toString(btnMesas.));
 }
